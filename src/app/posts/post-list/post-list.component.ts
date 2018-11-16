ad381e8f6f3c62169c2961315e7ac38eb1d377fc
mahdouglass <mahdouglass@gmail.com>
2018-11-16 07:54:50 -0700

    Added hard coded posts and set up structure to repeat posts if any are showing

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -6,12 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent {
-  posts = [
-    { title: 'First Post', content: 'This is the content of the first post.' },
-    {
-      title: 'Second Post',
-      content: 'This is the content of the second post.'
-    },
-    { title: 'Third Post', content: 'This is the content of the third post.' }
-  ];
+  // posts = [
+  //   { title: 'First Post', content: 'This is the content of the first post.' },
+  //   {
+  //     title: 'Second Post',
+  //     content: 'This is the content of the second post.'
+  //   },
+  //   { title: 'Third Post', content: 'This is the content of the third post.' }
+  // ];
+  posts = [];
 }
