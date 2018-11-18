53a6b8398c79f7d933bad65a2a22289c1624e645
mahdouglass <mahdouglass@gmail.com>
2018-11-18 06:11:38 -0700

    Created post.model.ts and added Post type to app and all post components

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 
+import { Post } from '../posts.model';
+
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
@@ -14,5 +16,5 @@ export class PostListComponent {
   //   },
   //   { title: 'Third Post', content: 'This is the content of the third post.' }
   // ];
-  @Input() posts = [];
+  @Input() posts: Post[] = [];
 }
