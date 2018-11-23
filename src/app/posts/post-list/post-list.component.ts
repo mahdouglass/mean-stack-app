f22e3f3fac9c80511babcb2c0a21a9bba2142238
mahdouglass <mahdouglass@gmail.com>
2018-11-23 14:42:15 -0700

    Added HttpClient to pull posts into webpage

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -10,21 +10,13 @@ import { PostsService } from '../posts.service';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit, OnDestroy {
-  // posts = [
-  //   { title: 'First Post', content: 'This is the content of the first post.' },
-  //   {
-  //     title: 'Second Post',
-  //     content: 'This is the content of the second post.'
-  //   },
-  //   { title: 'Third Post', content: 'This is the content of the third post.' }
-  // ];
   posts: Post[] = [];
   private postsSub: Subscription;
 
   constructor(public postsService: PostsService) {}
 
   ngOnInit() {
-    this.posts = this.postsService.getPosts();
+    this.postsService.getPosts();
     this.postsSub = this.postsService
       .getPostUpdateListener()
       .subscribe((posts: Post[]) => {
