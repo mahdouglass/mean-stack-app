1b8b7970e03c4d761842e870bf6061a4d470ab34
mahdouglass <mahdouglass@gmail.com>
2018-12-06 11:04:18 -0700

    Setting up ability to edit. Subscribing to paramMap to determine if we can get postId to edit and default to create if there is none.

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,5 @@
-import { Component, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 import { NgForm } from '@angular/forms';
 import { PostsService } from '../posts.service';
@@ -8,11 +9,26 @@ import { PostsService } from '../posts.service';
   templateUrl: './post-create.component.html',
   styleUrls: ['./post-create.component.css']
 })
-export class PostCreateComponent {
+export class PostCreateComponent implements OnInit {
   enteredTitle = '';
   enteredContent = '';
+  private mode = 'create';
+  private postId: string;
 
-  constructor(public postsService: PostsService) {}
+  constructor(public postsService: PostsService, public route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if(paramMap.has('postId')) {
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
+      }
+      else {
+        this.mode = 'create';
+        this.postId = null;
+      }
+    });
+  } 
 
   onAddPost(form: NgForm) {
     if (form.invalid) {
