0dbf68eddd5e06d475ba27b551fe48e00cb4ccd9
Marissa Douglass <mahdouglass@gmail.com>
2018-12-29 15:22:29 -0500

    Added page event for users to select how many posts to see per page.

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
+import { PageEvent } from "@angular/material";
 import { Subscription } from 'rxjs';
 
 import { Post } from "../posts.model";
@@ -14,6 +15,7 @@ export class PostListComponent implements OnInit, OnDestroy {
   isLoading = false;
   totalPosts = 10;
   postsPerPage = 5;
+  pageSizeOptions = [1,2,5,10];
   private postsSub: Subscription;
 
   constructor(public postsService: PostsService) {}
@@ -26,6 +28,10 @@ export class PostListComponent implements OnInit, OnDestroy {
         this.isLoading = false;
         this.posts = posts;
       });
+  }
+
+  onChangedPage(pageData: PageEvent) {
+    console.log(pageData);
   }
 
   onDelete(postId: string) {
