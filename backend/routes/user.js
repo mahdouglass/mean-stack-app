306680d861e1e7269bb9bf149b8098f25fc112ea
Marissa Douglass <mahdouglass@gmail.com>
2019-01-06 20:29:58 -0500

    Added user mongoose model and exported it.

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/signup", (req, res, next) => {
-    
+    // create new user and store in database
 });
 
 module.exports = router;
