8db6711db34dfca1cd64fd8e3a6f2e6b85919ce4
Marissa Douglass <mahdouglass@gmail.com>
2019-01-12 22:34:06 -0500

    Fixed uppercase User issues and created variable to store fetched user.

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -7,57 +7,59 @@ const User = require("../models/user");
 const router = express.Router();
 
 router.post("/signup", (req, res, next) => {
-    bcrypt.hash(req.body.password, 10).then(hash => {
-      const user = new User({
-        email: req.body.email,
-        password: hash
-      });
-      user
-        .save()
-        .then(result => {
-          res.status(201).json({
-            message: "User created!",
-            result: result
-          });
-        })
-        .catch(err => {
-          res.status(500).json({
-            error: err
-          });
-        });
+  bcrypt.hash(req.body.password, 10).then(hash => {
+    const user = new User({
+      email: req.body.email,
+      password: hash
     });
-  });
-
-  router.post("/login", (req, res, next) => {
-    User.findOne({ email: req.body.email })
-    .then(user => {
-      if (!User) {
-        return res.status(401).json({
-          message: 'Auth failed'
+    user
+      .save()
+      .then(result => {
+        res.status(201).json({
+          message: "User created!",
+          result: result
         });
-      }
-      return bcrypt.compare(req.body.password, User.password);
-    })
-    .then(result => {
-      if (!result) {
-        return res.status(401).json({
-          message: 'Auth failed'
-        });
-      }
-      const token = jwt.sign({email: user.email, userId: user._id},
-        'secret_this_should_be_longer_but_testing', 
-        { expiresIn: '1h' }
-      );
-      res.status(200).json({
-        message: 'Auth success',
-        token: token
       })
-    })
-    .catch(err => {
+      .catch(err => {
+        res.status(500).json({
+          error: err
+        });
+      });
+  });
+});
+
+router.post("/login", (req, res, next) => {
+  let fetchedUser;
+  User.findOne({ email: req.body.email })
+  .then(user => {
+    if (!user) {
       return res.status(401).json({
         message: 'Auth failed'
-      })
-    });
+      });
+    }
+    fetchedUser = user;
+    return bcrypt.compare(req.body.password, user.password);
+  })
+  .then(result => {
+    if (!result) {
+      return res.status(401).json({
+        message: 'Auth failed'
+      });
+    }
+    const token = jwt.sign({email: fetchedUser.email, userId: fetchedUser._id},
+      'secret_this_should_be_longer_but_testing', 
+      { expiresIn: '1h' }
+    );
+    res.status(200).json({
+      message: 'Auth success',
+      token: token
+    })
+  })
+  .catch(err => {
+    return res.status(401).json({
+      message: 'Auth failed'
+    })
   });
+});
 
 module.exports = router;
