8a7377956497ce80688a98b9ed0041da93ca41c7
Marissa Douglass <mahdouglass@gmail.com>
2019-01-18 21:31:15 -0500

    Added logout function.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -22,4 +22,8 @@ export class HeaderComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     this.authListenerSubs.unsubscribe();
   }
+
+  onLogout() {
+    this.authService.logout();
+  }
 }
