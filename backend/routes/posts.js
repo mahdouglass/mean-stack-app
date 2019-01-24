1b751bb5392d3033cc9d28e80bdbb88938254f94
Marissa Douglass <mahdouglass@gmail.com>
2019-01-24 08:57:09 -0500

    In posts route, check if creator is the same as current one to update post. Throw error if not authorized.

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -59,8 +59,13 @@ router.put("/:id", checkAuth, multer({storage: storage}).single("image"), (req, 
     content: req.body.content,
     imagePath: imagePath
   });
-  Post.updateOne({ _id: req.params.id }, post).then(result => {
-    res.status(200).json({ message: "Update successful!" });
+  Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post).then(result => {
+    if (result.nModified > 0) {
+      res.status(200).json({ message: "Update successful!" });
+    }
+    else {
+      res.status(401).json({ message: "Not authorized!" });
+    }
   });
 });
 
