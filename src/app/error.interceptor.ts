3945d9eb588fa425737d0367d806476a6a5b0a4c
Marissa Douglass <mahdouglass@gmail.com>
2019-01-29 21:26:30 -0500

    Fixed error in error interceptor...missed wrapping parentheses.

--- src/app/error.interceptor.ts
+++ src/app/error.interceptor.ts
@@ -5,11 +5,12 @@ import { throwError } from 'rxjs';
 export class ErrorInterceptor implements HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler) {
-       return next.handle(req).pipe();
-       catchError((error: HttpErrorResponse) => {
-           console.log(error);
-           alert(error.error.message);
-           return throwError(error);
-       });
+       return next.handle(req).pipe(
+            catchError((error: HttpErrorResponse) => {
+                console.log(error);
+                alert(error.error.message);
+                return throwError(error);
+            })
+       );
     }
 }
