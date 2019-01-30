c39cf8d28c71bf3db1e72dd8eeeeb2291575bbe5
Marissa Douglass <mahdouglass@gmail.com>
2019-01-29 21:07:50 -0500

    Created error interceptor for the whole app.

---
+++ src/app/error.interceptor.ts
@@ -0,0 +1,7 @@
+import { HttpInterceptor, HttpRequest, HttpHandler } from "@angular/common/http";
+
+export class AuthInterceptor implements HttpInterceptor {
+
+    intercept(req: HttpRequest<any>, next: HttpHandler) {
+       return next.handle(req);
+}
