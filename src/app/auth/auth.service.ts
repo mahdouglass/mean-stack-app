f51c08351d9dc0c27d8b21786ee5ed62aa7ecb79
Marissa Douglass <mahdouglass@gmail.com>
2019-02-11 20:37:17 -0500

    Added global environment variable for api url in the auth service.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,7 +3,10 @@ import { HttpClient } from "@angular/common/http";
 import { Router } from "@angular/router";
 import { Subject } from "rxjs";
 
+import { environment } from "../../environments/environment";
 import { AuthData } from "./auth-data.model";
+
+const BACKEND_URL = environment.apiUrl;
 
 @Injectable({ providedIn: "root" })
 
@@ -34,7 +37,7 @@ export class AuthService {
 
     createUser(email: string, password: string) {
         const authData: AuthData = {email: email, password: password};
-        return this.http.post("http://localhost:3000/api/user/signup", authData).subscribe(() => {
+        return this.http.post(BACKEND_URL + "/signup", authData).subscribe(() => {
             this.router.navigate["/"];
         }, error => {
             this.authStatusListener.next(false);
@@ -43,7 +46,7 @@ export class AuthService {
     
     login(email: string, password: string) {
         const authData: AuthData = {email: email, password: password};
-        this.http.post<{token: string, expiresIn: number, userId: string}>("http://localhost:3000/api/user/login", authData)
+        this.http.post<{token: string, expiresIn: number, userId: string}>(BACKEND_URL + "/login", authData)
             .subscribe(response => {
                 const token = response.token;
                 this.token = token;
