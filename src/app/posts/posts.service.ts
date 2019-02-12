f2d2ce0c9839d7a3246893e19791788e0dbb622c
Marissa Douglass <mahdouglass@gmail.com>
2019-02-11 20:33:47 -0500

    Changed posts service const to global environment variable.

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -4,9 +4,10 @@ import { Subject } from "rxjs";
 import { map } from "rxjs/operators";
 import { Router } from "@angular/router";
 
+import { environment } from "../../environments/environment";
 import { Post } from "./posts.model";
 
-const BACKEND_URL = "http://localhost:3000/api/posts";
+const BACKEND_URL = environment.apiUrl + "/posts";
 
 @Injectable({ providedIn: "root" })
 export class PostsService {
